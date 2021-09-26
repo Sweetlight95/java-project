be1df5e00819704f188e1d0574b97cb79b60a1c7
Sweetlight95 <princessadesuwa93@gmail.com>
2021-09-26 18:53:29 +0100

    progress

--- src/musicplayer/Mp3.java
+++ src/musicplayer/Mp3.java
@@ -1,4 +1,24 @@
 package musicplayer;
 
 public class Mp3 {
+    private boolean isOn;
+
+    public boolean isOn() {
+        return isOn;
+    }
+    public void turnOn(boolean isOn) {
+        this.isOn = isOn;
+    }
+
+    public void turnOff(boolean isOff) {
+        this.isOn = isOff;
+    }
+
+    public boolean isOff() {
+        return isOn;
+    }
+
+    public boolean playMusic(boolean playMusic) {
+        return false;
+    }
 }
--- test/musicplayer/Mp3Test.java
+++ test/musicplayer/Mp3Test.java
@@ -1,18 +1,33 @@
 package musicplayer;
 
 import exercise.MpThreePlayer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Mp3Test {
+    Mp3 sharp;
+    @BeforeEach
+    public void setUp() {
+        sharp = new Mp3();
+    }
     @Test
     void testMp3PlayerCanTurnOn() {
-        Mp3Test sharp = new Mp3Test();
         assertFalse(sharp.isOn());
-
-        sharp.turnOn();
-        assertTrue(isOn());
+        sharp.turnOn(true);
+        assertTrue(sharp.isOn());
     }
-
+    @Test
+    void testMp3PlayerCanTurnOff() {
+        sharp.turnOn(true);
+        assertTrue(sharp.isOn());
+        sharp.turnOff(true);
+        assertTrue(sharp.isOff());
+    }
+    @Test
+    void testMp3PlayerCanPlayMusic() {
+        sharp.playMusic(true);
+        assertTrue(sharp.playMusic(true));
+    }
 }
