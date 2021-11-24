4cdf75e675f8131e6b98e91b6a135a4cc1b30f23
Sweetlight95 <princessadesuwa93@gmail.com>
2021-11-24 11:56:15 +0100

    up

--- src/chapter17/RandomInterger.java
+++ src/chapter17/RandomInterger.java
@@ -1,4 +1,18 @@
 package chapter17;
 
+import java.security.SecureRandom;
+import java.util.stream.Collectors;
+
 public class RandomInterger {
+    public static void main(String[] args) {
+        SecureRandom randomNumbers = new SecureRandom();
+
+        System.out.println("Random numbers on separate lines:");
+        randomNumbers.ints(10, 1, 7).forEach(System.out::println);
+
+        String numbers =randomNumbers.ints(10, 1, 7)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+        System.out.printf("%nRandom numbers on one line: %s%n", numbers);
+    }
 }
